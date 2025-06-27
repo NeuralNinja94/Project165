7cb815dee2c6d8544b7c6fde462c708633688828
tu-shang <dusan06stevic@gmail.com>
2025-06-27 13:17:46 +0200

    Initial project import

---
+++ src/main/java/com/fundgrube/FundgrubeApplication.java
@@ -0,0 +1,13 @@
+package com.fundgrube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundgrubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundgrubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundgrube/FundgrubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundgrube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundgrubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
